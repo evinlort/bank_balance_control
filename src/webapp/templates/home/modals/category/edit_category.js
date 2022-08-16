43559594c17defde8a3c5e70c18a0757e01a3db6
Evgeny Grebnev <egrebnev@infinidat.com>
2022-08-16 23:07:57 +0300

    Show/Hide add subcat

--- src/webapp/templates/home/modals/category/edit_category.js
+++ src/webapp/templates/home/modals/category/edit_category.js
@@ -1,3 +1,23 @@
 $("#edit-category").on('shown.bs.modal', () => {
-        $(this).find('#edit_category_name').focus();
+    $(this).find('#edit_category_name').focus();
+})
+
+$("#edit-category").on('hidden.bs.modal', () => {
+    var subcat_name = $("#subcat_name")
+    subcat_name.val("")
+    subcat_name.addClass("d-none")
+})
+
+$("#show_add_subcat").on("click", (e) => {
+    var subcat_name = $("#subcat_name")
+    subcat_name.val("")
+
+    if (! subcat_name.is(":visible")) {
+        subcat_name.removeClass("d-none")
+        subcat_name.focus()
+    }
+    else {
+        subcat_name.addClass("d-none")
+        subcat_name.blur()
+    }
 })
