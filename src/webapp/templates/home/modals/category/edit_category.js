162991bd500e4240044dfb008d0ee31b2f618bb0
Evgeny Grebnev <egrebnev@infinidat.com>
2022-08-16 23:10:58 +0300

    Toggle button title

--- src/webapp/templates/home/modals/category/edit_category.js
+++ src/webapp/templates/home/modals/category/edit_category.js
@@ -10,14 +10,17 @@ $("#edit-category").on('hidden.bs.modal', () => {
 
 $("#show_add_subcat").on("click", (e) => {
     var subcat_name = $("#subcat_name")
+    var edit_category_button = $("#edit_category_button")
     subcat_name.val("")
 
     if (! subcat_name.is(":visible")) {
         subcat_name.removeClass("d-none")
         subcat_name.focus()
+        edit_category_button.text("Add new sub category")
     }
     else {
         subcat_name.addClass("d-none")
         subcat_name.blur()
+        edit_category_button.text("Edit category")
     }
 })
