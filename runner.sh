a67beb720135faea476e99dc1a0430182ccc296b
Evgeny Grebnev <egrebnev@infinidat.com>
2022-08-18 23:07:59 +0300

    set virtual env prior to run runner

--- runner.sh
+++ runner.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+APP=bank_balance_control
 export FLASK_APP=src
 export SECRET_KEY='Once there is a way to get back homeward'
 
+source $HOME/$APP/venv/bin/activate
 flask run --host=0.0.0.0 --port=8080
