ba2fb76bf9e52b6e43a7430dcc6e810d23a74a67
Evgeny Grebnev <egrebnev@infinidat.com>
2022-08-21 10:33:45 +0300

    Runner - via gunicorn

--- runner.sh
+++ runner.sh
@@ -5,4 +5,5 @@ export FLASK_APP=src
 export SECRET_KEY='Once there is a way to get back homeward'
 
 source $HOME/$APP/venv/bin/activate
-flask run --host=0.0.0.0 --port=8080
+#flask run --host=0.0.0.0 --port=8080
+gunicorn -w 4 -b 0.0.0.0:8080 'src:app'
