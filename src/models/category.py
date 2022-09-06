d5a6c58ceb738baaa2fea4f98795971c3ad3738c
Evgeny Grebnev <egrebnev@infinidat.com>
2022-09-06 10:11:20 +0300

    Save category preparation

--- src/models/category.py
+++ src/models/category.py
@@ -1,3 +1,7 @@
+import psycopg2.errors
+from psycopg2 import sql
+
+from src.models.name import Name
 from src.models.translation import Translation
 from src.models.base_model import BaseModel
 
@@ -18,3 +22,26 @@ class Category(BaseModel):
             )
             category["name"] = translation["translation"]
         return categories
+
+    def save(self, row):
+        names = Name(self.db)
+        name_id = names.save(row)
+        row["name_id"] = name_id
+
+        translation_model = Translation(self.db)
+        translation_model.save(row)
+
+        query = sql.SQL("INSERT INTO {} "
+                        "(name_id, created_by, creation_date) "
+                        "VALUES (%(name_id)s, %(created_by)s, %(creation_date)s) "
+                        "RETURNING id".format(self.table))
+        self.logger.info(self.db.cursor.mogrify(query, row))
+        try:
+            self.db.cursor.execute(query, row)
+            self.db.connection.commit()
+        except psycopg2.errors.UniqueViolation:
+            self.db.connection.rollback()
+            query = sql.SQL("SELECT id FROM {} WHERE name_id = %(name_id)s".format(self.table))
+            self.db.cursor.execute(query, row)
+
+        return self.db.cursor.fetchone()[0]
