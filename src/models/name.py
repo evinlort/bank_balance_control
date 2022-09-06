7fed61adabdd8dd94f69c60ea2ec7b06fb19f1ae
Evgeny Grebnev <egrebnev@infinidat.com>
2022-09-06 10:12:17 +0300

    Save name model

--- src/models/name.py
+++ src/models/name.py
@@ -1,3 +1,6 @@
+import psycopg2.errors
+from psycopg2 import sql
+
 from src.models.base_model import BaseModel
 
 
@@ -5,3 +8,20 @@ class Name(BaseModel):
     @staticmethod
     def get_table():
         return "names"
+
+    def save(self, row):
+        query = sql.SQL("INSERT INTO {} "
+                        "(name) "
+                        "VALUES (%(name)s) "
+                        "RETURNING id".format(self.table, self.table))
+        self.logger.info(self.db.cursor.mogrify(query, row))
+        try:
+            self.db.cursor.execute(query, row)
+            self.db.connection.commit()
+        except psycopg2.errors.UniqueViolation:
+            self.db.connection.rollback()
+            query = sql.SQL("SELECT id FROM {} WHERE name = %(name)s".format(self.table))
+            self.db.cursor.execute(query, row)
+
+        return self.db.cursor.fetchone()[0]
+
