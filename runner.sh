e6f7e98df9eb63103a0bfbfc4c9097f2ead4095c
Evgeny Grebnev <egrebnev@infinidat.com>
2022-09-26 23:08:04 +0300

    MOdify runner

--- runner.sh
+++ runner.sh
@@ -4,6 +4,11 @@ APP=bank_balance_control
 export FLASK_APP=src
 export SECRET_KEY='Once there is a way to get back homeward'
 
-#source $HOME/$APP/venv/bin/activate
+rm -rf $HOME/$APP/venv
+python3 -m pip install virtualenv
+python3 -m virtualenv venv
+
+source $HOME/$APP/venv/bin/activate
+pip3 install -r requirements.txt
 #flask run --host=0.0.0.0 --port=8080
 gunicorn -w 1 -b 0.0.0.0:8080 'src:app'
