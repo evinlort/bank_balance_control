af71032f39efe12e94074b37437735aa7f6356ec
evinlort <evgeny.gr81@gmail.com>
2022-10-18 10:00:52 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+DIR=$HOME
+#DIR=/opt
 APP=bank_balance_control
 export FLASK_APP=src
 export SECRET_KEY='Once there is a way to get back homeward'
 
-rm -rf $HOME/$APP/venv
+rm -rf $DIR/$APP/venv
 python3 -m pip install virtualenv
 python3 -m virtualenv venv
 
-source $HOME/$APP/venv/bin/activate
+source $DIR/$APP/venv/bin/activate
 pip3 install -r requirements.txt
 #flask run --host=0.0.0.0 --port=8080
 gunicorn -w 1 -b 0.0.0.0:8080 'src:app'
