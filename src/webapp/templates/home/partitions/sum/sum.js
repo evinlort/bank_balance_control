ccadf6a372d5c16f3f909ccf532b22a9c709d379
Evgeny Grebnev <egrebnev@infinidat.com>
2022-11-19 20:51:24 +0200

    BBS-1 - Drop leading zero if after it goes a number

--- src/webapp/templates/home/partitions/sum/sum.js
+++ src/webapp/templates/home/partitions/sum/sum.js
@@ -6,9 +6,17 @@ $("#sum").on("keyup", (e) => {
     if (isNaN(e.target.value)) {
         sum.val(sum.val().slice(0, -1))
     }
+    else if (sum.val() === "00") {
+        sum.val(sum.val().slice(0, -1))
+    }
     else {
         if (sum.val().search(/\./) > 0 && sum.val().split(".")[1].length > 2) sum.val(sum.val().slice(0, -1))
     }
+
+    if (sum.val().length > 1 && sum.val()[0] === "0") {
+        sum.val(sum.val().slice(1))
+    }
+
     if (sum.val() == "") {
         first_payment.text("")
         first_payment_label.addClass("d-none")
