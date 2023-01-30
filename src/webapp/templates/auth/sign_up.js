21a2501f248b9390d052d513aadcf35f03ee6f85
Evgeny Grebnev <egrebnev@infinidat.com>
2023-01-30 21:35:41 +0200

    BBC-19 - 120 years range (birthdate)

--- src/webapp/templates/auth/sign_up.js
+++ src/webapp/templates/auth/sign_up.js
@@ -1,9 +1,10 @@
 $("#day_of_birth_parsed").datepicker({
     changeMonth: true,
     changeYear: true,
-    minDate: -31,
+    minDate: "-120Y",
     maxDate: "+0D",
     altField: "#day_of_birth",
     altFormat: "yy-mm-dd",
-    dateFormat: "dd/mm/yy"
+    dateFormat: "dd/mm/yy",
+    yearRange: "c-120:c+0"
 }).datepicker('setDate', '+0d')
