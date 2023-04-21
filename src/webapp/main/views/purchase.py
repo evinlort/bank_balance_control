80d0402918c42bd1c45a0c688f37bc880d8efdb2
Evgeny Grebnev <egrebnev@infinidat.com>
2023-04-21 15:07:05 +0300

    BBC-12 - Build purchases list with additional data (user and category)

--- src/webapp/main/views/purchase.py
+++ src/webapp/main/views/purchase.py
@@ -2,6 +2,7 @@ from flask import render_template
 from flask_login import login_required, current_user
 
 from src.config import logger
+from src.helpers.models_helper import add_object_by_name
 from src.models import (
     db, Purchase,
 )
@@ -13,7 +14,11 @@ from src.webapp.main import main
 def show_purchases():
     purchase = Purchase(db)
     logger.error(current_user.__dict__)
+
     purchases = purchase.get_all()
-    logger.info(purchases)
+    add_object_by_name(db, "user", purchases)
+    add_object_by_name(db, "category", purchases)
+
+    logger.debug(purchases)
     params = {"purchases": purchases}
     return render_template("/purchases/purchases.html", params=params)
