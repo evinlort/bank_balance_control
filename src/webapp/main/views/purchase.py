c5ee280612ad0b31600496bbacdf9ab7598634ee
Evgeny Grebnev <egrebnev@infinidat.com>
2023-04-23 23:35:34 +0300

    BBC-12 - add user and its family members

--- src/webapp/main/views/purchase.py
+++ src/webapp/main/views/purchase.py
@@ -1,10 +1,12 @@
+import datetime
+
 from flask import render_template
 from flask_login import login_required, current_user
 
 from src.config import logger
 from src.helpers.models_helper import add_object_by_name
 from src.models import (
-    db, Purchase,
+    db, Purchase, User,
 )
 from src.webapp.main import main
 
@@ -13,12 +15,24 @@ from src.webapp.main import main
 @login_required
 def show_purchases():
     purchase = Purchase(db)
-    logger.error(current_user.__dict__)
+    this_user = current_user.__dict__
+
+    this_user["family_members"] = None
+    if this_user["family_id"]:
+        this_user["family_members"] = User(db).get_by_column("family_id", this_user["family_id"])
 
     purchases = purchase.get_all()
-    add_object_by_name(db, "user", purchases)
+    add_object_by_name(db, "means_of_payment", purchases)
     add_object_by_name(db, "category", purchases)
+    add_object_by_name(db, "user", purchases)
 
-    logger.debug(purchases)
-    params = {"purchases": purchases}
+    current_date = datetime.datetime.now()
+    month_name = current_date.strftime("%B")
+
+    params = {
+        "purchases": purchases,
+        "month": month_name,
+        "current_user": this_user,
+    }
+    logger.debug(this_user)
     return render_template("/purchases/purchases.html", params=params)
