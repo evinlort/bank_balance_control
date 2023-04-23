042b1d70b6fc71c7f8d05335157d78ca7b15a972
Evgeny Grebnev <egrebnev@infinidat.com>
2023-04-23 23:13:49 +0300

    BBC-12 - add_object_by_name uses cache

--- src/helpers/models_helper.py
+++ src/helpers/models_helper.py
@@ -1,9 +1,16 @@
 import importlib
+from typing import List
 
 
-def add_object_by_name(db_obj, name, items):
+def add_object_by_name(db_obj, name: str, items: List[dict]):
     model_name = importlib.import_module(f"src.models")
-    model = getattr(model_name, name.capitalize())
+    object_name = "".join([name_part.capitalize() for name_part in name.split("_")])
+    model = getattr(model_name, object_name)
+    cache_name_id = {}
     for item in items:
         if f"{name}_id" in item:
-            item[name] = model(db_obj).get_by_id(item[f"{name}_id"])
+            if item[f"{name}_id"] in cache_name_id:
+                item[name] = cache_name_id[item[f"{name}_id"]]
+            else:
+                item[name] = model(db_obj).get_by_id(item[f"{name}_id"])
+                cache_name_id[item[f"{name}_id"]] = item[name]
