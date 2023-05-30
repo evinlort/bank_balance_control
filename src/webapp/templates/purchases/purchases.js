f3c9f02ec83479a0b8ec8508acf6607a96898813
Evgeny Grebnev <egrebnev@infinidat.com>
2023-05-30 22:00:44 +0300

    BBC-12 Hide all except the clicked collapse

--- src/webapp/templates/purchases/purchases.js
+++ src/webapp/templates/purchases/purchases.js
@@ -1,1 +1,14 @@
 log("Test purchases")
+
+$("[id^=parent-collapse-purchase-]").each((index, element) => {
+    $(element).on("click", () => {
+        disable_other_collapses_except(index+1)
+    })
+})
+
+disable_other_collapses_except = (index) => {
+    $("[id^='collapse-purchase-'][id!='collapse-purchase-" + index + "']").each((index, element) => {
+        log(element)
+        $(element).collapse("hide")  // Hide all except the clicked collapse
+    })
+}
